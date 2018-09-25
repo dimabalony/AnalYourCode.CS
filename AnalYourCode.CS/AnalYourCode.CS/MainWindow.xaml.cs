383bdc07415545769e031f2ad40555a11122357c
Maxim Krouk <maximkrouk@gmail.com>
2018-09-25 21:10:10 +0300

    Project.start

---
+++ AnalYourCode.CS/AnalYourCode.CS/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnalYourCode.CS
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void ButtonChangeWS_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonHelp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonAnalyze_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
